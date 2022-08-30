246c9ed781345d15da4d71eac9710665d47aa9f5
Piper McCorkle <contact@piperswe.me>
2022-08-29 21:47:23 -0500

    Add a check

--- flake.nix
+++ flake.nix
@@ -1,8 +1,24 @@
 {
   description = "A NixOS module for cloudflared";
 
-  outputs = { self }: rec {
+  outputs = { self, nixpkgs }: rec {
     nixosModules.cloudflared = import ./module.nix;
     nixosModules.default = nixosModules.cloudflared;
+
+    checks.x86_64-linux.cloudflared =
+      let
+        module = { modulesPath, ... }: {
+          imports = [
+            "${modulesPath}/installer/cd-dvd/iso-image.nix"
+          ];
+          services.cloudflared.enable = true;
+          system.stateVersion = "22.05";
+        };
+        system = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ nixosModules.cloudflared module ];
+        };
+      in
+      system.config.system.build.toplevel;
   };
 }
