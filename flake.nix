926d70424c161f0ffea460cfae80992c4d6d8580
Piper McCorkle <contact@piperswe.me>
2025-10-06 17:32:46 +0000

    add a test for the container config

--- flake.nix
+++ flake.nix
@@ -20,5 +20,44 @@
         };
       in
       system.config.system.build.toplevel;
+    checks.x86_64-linux.cloudflared-container =
+      let
+        module = { modulesPath, ... }: {
+	  imports = [
+	    "${modulesPath}/installer/cd-dvd/iso-image.nix"
+	  ];
+
+	  networking.nat = {
+            enable = true;
+            internalInterfaces = [ "ve-+" ];
+            externalInterface = "enp8s0";
+          };
+
+          # Create a container for cloudflared
+          containers.cloudflared = {
+            privateNetwork = true;
+            hostAddress = "192.168.100.2";
+            localAddress = "192.168.100.12";
+            ephemeral = true;
+            autoStart = true;
+            bindMounts = {
+              "/var/lib/cloudflared" = {
+                hostPath = "/var/lib/cloudflared";
+              };
+            };
+            config = { config, pkgs, ... }: {
+              imports = [ nixosModules.cloudflared ];
+              services.cloudflared.enable = true;
+              system.stateVersion = "22.05";
+            };
+          };
+          system.stateVersion = "22.05";
+	};
+	system = nixpkgs.lib.nixosSystem {
+	  system = "x86_64-linux";
+	  modules = [ module ];
+	};
+      in
+      system.config.system.build.toplevel;
   };
 }
